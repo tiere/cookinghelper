7c1f817addf68187e662ab0ad5bc02c326a0e70f
tiere <tommi.tiere@gmail.com>
2013-09-23 21:18:39 +0300

    Some tests for ingredient model

--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @ingredient = ingredients(:tomato)
+  end
+  test "should save with proper values" do
+    assert @ingredient.valid?, "Ingredient is not valid"
+    assert @ingredient.save, "Ingredient was not saved"
+  end
+
+  test "should not save without a name" do
+    @ingredient.name = nil
+
+    refute @ingredient.valid?, "Ingredient is valid"
+    refute @ingredient.save, "Ingredient was saved"
+  end
+
+  test "should not save with a too short name" do
+    @ingredient.name = 'a'
+
+    refute @ingredient.valid?, "Ingredient is valid"
+    refute @ingredient.save, "Ingredient was saved"
+  end
+
+  test "should not save with too long name" do
+    @ingredient.name = 'b'*31
+
+    refute @ingredient.valid?, "Ingredient is valid"
+    refute @ingredient.save, "Ingredient was saved"
+  end
 end
