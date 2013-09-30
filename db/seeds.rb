e2542f8a365a0dc4c9faa7426aa7232ea814498b
tiere <tommi.tiere@gmail.com>
2013-09-30 22:40:22 +0300

    Started adding new seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 foodstuffs = Foodstuff.create([
-                                { name: 'Tomato' },
-                                { name: 'Banana' },
-                                { name: 'Flour' },
-                                { name: 'Salt' }
+  { name: 'Tomato' },
+  { name: 'Banana' },
+  { name: 'Flour' },
+  { name: 'Salt' }
 ])
 
 units = Unit.create([
-                      { name: 'Kilograms', abbreviation: "kg" },
-                      { name: 'Liters', abbreviation: "l" },
-                      { name: 'Grams', abbreviation: "g" },
-                      { name: 'Units', abbreviation: "kpl" }
+  { name: 'Kilograms', abbreviation: "kg" },
+  { name: 'Liters', abbreviation: "l" },
+  { name: 'Grams', abbreviation: "g" },
+  { name: 'Units', abbreviation: "kpl" }
 ])
 
 categories = Category.create([
@@ -26,3 +26,23 @@ categories = Category.create([
   { name: 'Salad' },
   { name: 'Main' }
 ])
+
+recipes = Recipe.create([
+  {
+    name: 'Cheese Cake', category: categories[1],
+    ingredients: [{ foodstuff: foodstuffs.first, quantity: 2, unit: units[3] }],
+    steps: [{ name: 'Boil', duration: 3600 }]
+  },
+  { name: 'Pasta Bolognese', category: categories[3] },
+  { name: 'Tuna salad', category: categories[2] }
+])
+
+# ingredients = Ingredient.create([
+#   { recipe: recipes.first, foodstuff: foodstuffs.first, quantity: 2, unit: units[3] },
+#   { recipe: recipes.first, foodstuff: foodstuffs[1], quantity: 2, unit: units[3] }
+# ])
+
+# steps = Step.create([
+#   { name: 'Boil', recipe: recipes.first, duration: 3600},
+#   { name: 'Slice onions', recipe: recipes.first, duration: 625}
+# ])
