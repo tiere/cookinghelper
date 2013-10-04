8b7b6f2830822b31fa62b2bfb21d49eee357d296
tiere <tommi.tiere@gmail.com>
2013-10-04 16:31:04 +0300

    Continued virtual time attributes of step

--- app/models/step.rb
+++ app/models/step.rb
@@ -11,11 +11,27 @@ class Step < ActiveRecord::Base
     numericality: { only_integer: true },
     inclusion: { in: 60..86400, message: 'must be between 60 and 86400' }
 
+  # def duration_h
+  #   case self
+  # end
+
+  def duration_s
+    self.duration.to_i.divmod(60)[1]
+  end
+
   def duration_m
-    self.duration.to_i / 60
+    self.duration_s.to_i.divmod(60)[1]
+  end
+
+  def duration_h
+    self.duration_m.to_i.divmod(60)[0]
   end
 
   def duration_m=(duration)
-    self.duration = duration.to_i * 60
+    self.duration += (duration.to_i * 60)
+  end
+
+  def duration_h=(duration)
+    self.duration += (duration.to_i * 60 * 60)
   end
 end
