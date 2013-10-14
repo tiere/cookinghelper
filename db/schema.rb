0b1a272fe89affd559078724020a19bde1c2dfe0
tiere <tommi.tiere@gmail.com>
2013-10-14 20:11:43 +0300

    Add secure password for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131014145321) do
+ActiveRecord::Schema.define(version: 20131014162900) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20131014145321) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
