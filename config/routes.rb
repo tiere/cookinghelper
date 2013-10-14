f31667faee948cc2e7b99c123a479cbc703d3689
tiere <tommi.tiere@gmail.com>
2013-10-14 16:31:27 +0300

    Add users_controller and some more tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Cookinghelper::Application.routes.draw do
-  get "home/index"
+  root 'home#index'
+  get "users/new"
+  match '/signup', to: 'users#new', via: 'get'
+  resources :recipes, except: [:destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
-
-  resources :recipes, except: [:destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -4,6 +4,20 @@ describe "Home Page" do
   subject { page }
   before { visit root_path }
 
-  it { should have_content("Cooking Helper") }
+  it { should have_selector('h1', text: "Cooking Helper") }
   it { should have_title(full_title('Home')) }
+
+  it "should have all links working" do
+    visit root_path
+
+    click_link "Cooking Helper"
+    expect(page).to have_title(full_title('Home'))
+
+    click_link "Recipes"
+    expect(page).to have_title(full_title('Recipes'))
+
+    click_link "Cooking Helper"
+    click_link "Sign up"
+    expect(page).to have_title(full_title('Sign up'))
+  end
 end
