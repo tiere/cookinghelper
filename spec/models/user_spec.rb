01b0978471291724d34320e72446a7e061f7dd0d
tiere <tommi.tiere@gmail.com>
2013-10-15 17:41:58 +0300

    Some improvements to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,12 +34,23 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[name@foo,com name_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[name@foo,com name_at_foo.org example.user@foo.
+        foo@bar_baz.com foo@bar+baz.com hemuli.tati@testi..fi]
 
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
       end
+    end
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Essi.Esimerkki@example.fi" }
+
+    it "should be saved as all lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
 
