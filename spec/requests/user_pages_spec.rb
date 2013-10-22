225bbd58d5a2e3fb22bbd31bc71a76e7eb12d4df
tiere <tommi.tiere@gmail.com>
2013-10-22 16:33:08 +0300

    Some refactoring to user registration

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_selector('small', text: "can't be blank") }
+      end
     end
 
     describe "with valid information" do
@@ -39,6 +46,15 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'pertti@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert-box.success',
+                                  text: 'Account successfully created') }
       end
     end
   end
