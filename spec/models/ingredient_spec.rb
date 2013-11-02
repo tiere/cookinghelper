6559e2bc9622fdb618f4fb254050bbfbad3b2b82
tiere <tommi.tiere@gmail.com>
2013-11-02 22:01:39 +0200

    Add tests for ingredient

---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Ingredient do
+  it { should respond_to(:quantity) }
+  it { should respond_to(:foodstuff) }
+  it { should respond_to(:recipe) }
+  it { should respond_to(:unit) }
+
+  it { should validate_presence_of(:quantity) }
+  it { should ensure_inclusion_of(:quantity).in_range(0.1..9999)
+       .with_message('must be between 0.1 and 9999') }
+  it { should_not allow_value('three').for(:quantity) }
+  it { should allow_value(100).for(:quantity) }
+
+  it { should validate_presence_of(:unit) }
+  it { should validate_presence_of(:foodstuff) }
+
+  it { should belong_to(:recipe) }
+  it { should belong_to(:foodstuff) }
+  it { should belong_to(:unit) }
+end
