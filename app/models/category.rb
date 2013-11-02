c5761a1f4c8ddf53e57f51f453e4771785754e4c
tiere <tommi.tiere@gmail.com>
2013-11-02 21:44:18 +0200

    Add tests for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,6 @@ class Category < ActiveRecord::Base
   validates :name,
     presence: true,
     length: { minimum: 2, maximum: 60 },
-    format: { with: /\A[a-zA-Z0-9 ]+\z/, message: "can only contain letters" }
-end
+    format: { with: /\A[a-zA-Z0-9 ]+\z/,
+              message: "can only contain letters and numbers" }
+    end
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Category do
+
+  it { should respond_to :name }
+  it { should respond_to :recipes }
+
+  it { should validate_presence_of(:name) }
+  it { should ensure_length_of(:name).is_at_least(2).is_at_most(60) }
+  it { should allow_value('Starters').for(:name) }
+  it { should_not allow_value('St$rt#rs').for(:name) }
+
+  it { should have_many(:recipes) }
+end
