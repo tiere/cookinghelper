c5c8c28813539186ef6837cc971f78ea66927f1f
tiere <tommi.tiere@gmail.com>
2013-11-06 16:17:22 +0200

    Specs for step and unit

--- app/models/step.rb
+++ app/models/step.rb
@@ -4,15 +4,18 @@ class Step < ActiveRecord::Base
   validates :name,
     presence: true,
     length: { minimum: 2, maximum: 60 },
-    format: { with: /\A[a-zA-Z0-9 ]+\z/, message: 'can only contain letters' }
+    format: { with: /\A[a-zA-Z0-9 ]+\z/,
+              message: 'can only contain letters and numbers' }
 
-  validates :duration,
+    validates :duration,
     presence: true,
     numericality: { only_integer: true },
     inclusion: { in: 60..86400, message: 'must be between 1 minute and 24 hours' }
 
   def duration_h
-    self.duration.to_i.divmod(60)[0].divmod(60)[0].divmod(60)[1] unless self.duration.nil?
+    unless self.duration.nil?
+      self.duration.to_i.divmod(60)[0].divmod(60)[0].divmod(60)[1]
+    end
   end
 
   def duration_m
@@ -24,26 +27,14 @@ class Step < ActiveRecord::Base
   end
 
   def duration_h=(duration)
-    if duration == ''
-      duration = 0
-    end
-
     @duration_h = duration
   end
 
   def duration_m=(duration)
-    if duration == ''
-      duration = 0
-    end
-
     @duration_m = duration
   end
 
   def duration_s=(duration)
-    if duration == ''
-      duration = 0
-    end
-
     @duration_s = duration
   end
 
@@ -53,62 +44,5 @@ class Step < ActiveRecord::Base
     seconds = @duration_s.to_i
 
     self.duration = hours + minutes + seconds
-  end
-
-  before_validation do
-    valid = false
-    if !@duration_h.nil?
-      valid = hour_is_valid?
-    elsif !@duration_m.nil?
-      valid = minute_is_valid?
-    elsif !@duration_s.nil?
-      valid = second_is_valid?
-    end
-
-    if valid
-      sum_durations
-    end
-  end
-
-  def hour_is_valid?
-    begin
-      if !Integer(@duration_h).between?(0, 24)
-        errors.add(:duration_h, "must be between 0 and 24")
-        false
-      else
-        true
-      end
-    rescue ArgumentError
-      errors.add(:duration_h, 'is not a number')
-      false
-    end
-  end
-
-  def minute_is_valid?
-    begin
-      if !Integer(@duration_m).between?(0, 59)
-        errors.add(:duration_m, "must be between 0 and 59")
-        false
-      else
-        true
-      end
-    rescue ArgumentError
-      errors.add(:duration_m, 'is not a number')
-      false
-    end
-  end
-
-  def second_is_valid?
-    begin
-      if !Integer(@duration_s).between?(0, 59)
-        errors.add(:duration_s, "must be between 0 and 59")
-        false
-      else
-        true
-      end
-    rescue ArgumentError
-      errors.add(:duration_s, 'is not a number')
-      false
-    end
   end
 end
